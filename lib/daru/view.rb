9e94d9184e07d0b0029d54c672e429c9e0e12f3d
shekharrajak <shekharstudy@ymail.com>
2017-06-11 13:49:15 +0530

    automatically load the JS when Daru::View.plotting_library is set

--- lib/daru/view.rb
+++ lib/daru/view.rb
@@ -14,6 +14,7 @@ module Daru
         else
           raise ArgumentError, "Unsupported library #{lib}"
         end
+        load_lib_in_iruby lib.to_s.capitalize if defined? IRuby
       end
 
       # Load the dependent JS files in IRuby notebook. Those JS will help in
@@ -29,11 +30,11 @@ module Daru
       # To load the HighCharts dependent JS
       # files (highcharts.js, highcharts-3d.js, highstock.js):
       #
-      # Daru::View.load_lib_in_iruby('HighCharts')
+      # Daru::View.load_lib_in_iruby('Highcharts')
       #
       def load_lib_in_iruby(library)
         require "daru/view/adapters/#{library}/iruby_notebook"
-        if library.match('HighCharts')
+        if library.match('Highcharts')
           library = 'LazyHighCharts'
         end
         Object.const_get(library).init_iruby
--- lib/daru/view/adapters/highcharts.rb
+++ lib/daru/view/adapters/highcharts.rb
@@ -10,6 +10,33 @@ module Daru
 
         # Read : https://www.highcharts.com/docs/chart-concepts to understand
         # the highcharts option concept.
+        #
+        # todo : this docs must be improved
+        # options :
+        # => :x
+        # => :y
+        # => :data
+        # => :type
+        # => :name
+        # => :chart
+        #       => :type
+        #       => :options3d
+        #       => :margin
+        # => :title
+        # => :subtitle
+        # => :plotOptions
+        #
+        # @param [Hash] options the options to create a chart with.
+        # @option opts [String] :title The chart title
+        # @option opts [String] :subtitle The chart subtitle
+        # @option opts [String/Symbol] :type The chart type
+        # @option opts [Daru::Vector / Array] :x X Axis data
+        # @option opts [Daru::Vector / Array] :y Y Axis data
+        # @option opts [Hash] :chart The chart options(there are many options in chart Hash, same as LazyHighCahrts or HighCharts)
+        # @option opts [Hash] :plotOptions The plot options, how the plot type is configured
+        #
+        # @param [Array/Daru::DataFrame/Daru::Vector] data
+        #
         def init(data, options)
           case
           when data.is_a?(Daru::DataFrame)
@@ -31,10 +58,12 @@ module Daru
           @chart = LazyHighCharts::HighChart.new('graph') do |f|
             # chart option may contains : :type, :options3d, :margin
             f.chart(options[:chart]) unless options[:chart].nil?
-            f.title({:text=> options[:title]}) unless options[:title].nil?
 
-            series_type = options[:type]
-            series_name = options[:name]
+            f.title({:text=> options[:title]}) unless options[:title].nil?
+            f.subtitle({:text=> options[:subtitle]}) unless options[:subtitle].nil?
+
+            series_type = options[:type] unless options[:type].nil?
+            series_name = options[:name] unless options[:name].nil?
             f.series(:type=> series_type, :name=> series_name, :data=> data)
 
             f.plotOptions(options[:plotOptions]) unless options[:plotOptions].nil?
