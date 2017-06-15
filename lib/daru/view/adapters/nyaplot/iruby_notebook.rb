e2fa68bc06bd01bda015657b56fe7321cfd4751b
shekharrajak <shekharstudy@ymail.com>
2017-06-16 01:20:29 +0530

    nyaplot js file and highcharts js files added. Nyaplot iruby notebook example added

--- lib/daru/view/adapters/nyaplot/iruby_notebook.rb
+++ lib/daru/view/adapters/nyaplot/iruby_notebook.rb
@@ -1,3 +1,22 @@
 module Nyaplot
+  # generate initializing code
+  def self.generate_init_code(dependent_js)
+    js_dir = File.expand_path("../../js/nyaplot_js", __FILE__)
+    path = File.expand_path("../../../templates/nyaplot/init.inline.js.erb", __FILE__)
+    template = File.read(path)
+    ERB.new(template).result(binding)
+  end
 
+  # Enable to show plots on IRuby notebook
+  def self.init_iruby(
+    dependent_js=['d3.min.js', 'd3-downloadable.js', 'nyaplot.js']
+  )
+    # todo: include highstock.js for highstock and modules/*.js files for
+    # exporting and getting data from various source like csv files etc.
+    #
+    # Highstock.js includes the highcharts.js, so only one of them required.
+    # see: https://www.highcharts.com/errors/16
+    js = self.generate_init_code(dependent_js)
+    IRuby.display(IRuby.javascript(js))
+  end
 end
