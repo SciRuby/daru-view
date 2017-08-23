206132b405e6f9985bb13d376d1cc2cb0d9efdbc
shekharrajak <shekharstudy@ymail.com>
2017-08-23 21:24:27 +0530

    daru_chart & daru_table helper methods for rails

---
+++ lib/daru/view/app/rails/railtie.rb
@@ -0,0 +1,16 @@
+require_relative 'helpers/view_helper'
+
+module Daru
+  module View
+    module Rails
+      class Railtie < ::Rails::Railtie
+
+        initializer "daru/view" do
+          ActiveSupport.on_load(:action_controller) do
+            include Daru::View::Rails::ViewHelper
+          end
+        end
+      end
+    end
+  end
+end
