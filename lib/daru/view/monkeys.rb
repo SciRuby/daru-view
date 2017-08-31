f2ba9d151216746fbc4eb9db3323b94e1334f0d3
shekharrajak <shekharstudy@ymail.com>
2017-08-31 23:10:53 +0530

    renaming #plot to #plot_view

--- lib/daru/view/monkeys.rb
+++ lib/daru/view/monkeys.rb
@@ -3,10 +3,10 @@ module Daru
     # @example
     #
     # df = Daru::DataFrame.new([1, 2, 3, 4, 5])
-    # df.plot(adapter: :googlecharts)
+    # df.plot_view(adapter: :googlecharts)
     #
     # Note: `options` can take options same as Daru::View::Plot(df, options)
-    def plot options={}
+    def plot_view options={}
       Daru::View::Plot.new(self, options)
     end
   end
