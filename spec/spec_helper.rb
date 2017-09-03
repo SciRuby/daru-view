e5d2d3ee041f34b1c1ccaf352d4f5d76fa2afd7d
shekharrajak <shekharstudy@ymail.com>
2017-09-03 17:53:33 +0530

    comment dummy_rails things in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,5 @@ require 'daru'
 # require 'nyaplot'
 
 # Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../dummy_rails/config/environment.rb",  __FILE__)
+# ENV["RAILS_ENV"] = "test"
+# require File.expand_path("../dummy_rails/config/environment.rb",  __FILE__)
