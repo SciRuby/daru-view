4f27875a32b1ef5aeee1b9c43900fb9fee94b8c5
shekharrajak <shekharrajak@live.com>
2017-09-05 17:06:58 +0530

    hint added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require 'rspec'
 require 'simplecov'
 require 'daru/view'
 require 'daru'
-# require 'nyaplot'
 
 # Configure Rails Environment
+# these lines are needed when spec/dummy_rails will be created
 # ENV["RAILS_ENV"] = "test"
 # require File.expand_path("../dummy_rails/config/environment.rb",  __FILE__)
