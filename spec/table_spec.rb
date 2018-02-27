e076d34718a6cbf8566ad5b4b041b9a45e1c2534
Prakriti Gupta <prakriti.gupta10@gmail.com>
2018-02-27 18:22:22 +0530

    Verify options and data attributes

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,6 +1,9 @@
 describe Daru::View::Table, 'Generating Table with Googlecharts library' do
   context 'initialize with Googlecharts library' do
-    before { Daru::View.table_library = :googlecharts }
+    before do
+      Daru::View.table_library = :googlecharts 
+    end
+    let(:options) {{width: 800, height: 720}}
     let(:lib) { Daru::View.table_library }
     let(:df) do
       Daru::DataFrame.new(
@@ -12,10 +15,11 @@ describe Daru::View::Table, 'Generating Table with Googlecharts library' do
       )
     end
     let(:dv) { Daru::Vector.new [1, 2, 3] }
-    let(:table_array) { Daru::View::Table.new(
-      [['col1', 'col2', 'col3'],[1, 2, 3]]) }
-    let(:table_df) { Daru::View::Table.new(df) }
-    let(:table_dv) { Daru::View::Table.new(dv) }
+    let(:array) {[['col1', 'col2', 'col3'],[1, 2, 3]]}
+    let(:table_df) { Daru::View::Table.new(df, options) }
+    let(:table_dv) { Daru::View::Table.new(dv, options) }
+    let(:table_array) { Daru::View::Table.new(array, options) }
+
     it 'check table library' do
       expect(lib).to eq(:googlecharts)
     end
@@ -23,23 +27,30 @@ describe Daru::View::Table, 'Generating Table with Googlecharts library' do
     it 'Googlecharts table using Array' do
       expect(table_array).to be_a Daru::View::Table
       expect(table_array.table).to be_a GoogleVisualr::DataTable
+      expect(table_array.options).to eq options
+      expect(table_array.data).to eq array
     end
 
     it 'Googlecharts table using DataFrame' do
       expect(table_df).to be_a Daru::View::Table
       expect(table_df.table).to be_a GoogleVisualr::DataTable
+      expect(table_df.options).to eq options
+      expect(table_df.data).to eq df
     end
 
     it 'Googlecharts table using Vector' do
       expect(table_dv).to be_a Daru::View::Table
       expect(table_dv.table).to be_a GoogleVisualr::DataTable
+      expect(table_dv.options).to eq options
+      expect(table_dv.data).to eq dv
     end
   end # initialize context end
 end
 
-describe Daru::View::Table, 'Generating Table with Daru-DataTable library' do
-  context 'initialize with Daru-DataTable library' do
+describe Daru::View::Table, 'Generating Table with daru-data_tables library' do
+  context 'initialize with daru-data_tables library' do
     before { Daru::View.table_library = :datatables }
+    let(:options) {{width: 800, height: 720}}
     let(:lib) { Daru::View.table_library }
     let(:df) do
       Daru::DataFrame.new(
@@ -51,20 +62,24 @@ describe Daru::View::Table, 'Generating Table with Daru-DataTable library' do
       )
     end
     let(:dv) { Daru::Vector.new [1, 2, 3] }
-    let(:table_df) { Daru::View::Table.new(df) }
-    let(:table_dv) { Daru::View::Table.new(dv) }
+    let(:table_df) { Daru::View::Table.new(df, options) }
+    let(:table_dv) { Daru::View::Table.new(dv, options) }
     it 'check table library' do
       expect(lib).to eq(:datatables)
     end
 
-    it 'Daru-DataTable table using DataFrame' do
+    it 'daru-data_tables table using DataFrame' do
       expect(table_df).to be_a Daru::View::Table
       expect(table_df.table).to be_a Daru::DataTables::DataTable
+      expect(table_df.options).to eq options
+      expect(table_df.data).to eq df
     end
 
-    it 'Daru-DataTable table using Vector' do
+    it 'daru-data_tables table using Vector' do
       expect(table_dv).to be_a Daru::View::Table
       expect(table_dv.table).to be_a Daru::DataTables::DataTable
+      expect(table_dv.options).to eq options
+      expect(table_dv.data).to eq dv
     end
   end # initialize context end
 end
