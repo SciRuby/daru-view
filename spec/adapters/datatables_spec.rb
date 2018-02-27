8d0559ad7dc9d05ed3b69d6c25136b69c9c23aca
Prakriti Gupta <prakriti.gupta10@gmail.com>
2018-02-27 17:53:06 +0530

    Change string array in datatables_spec.rb

--- spec/adapters/datatables_spec.rb
+++ spec/adapters/datatables_spec.rb
@@ -4,7 +4,7 @@ describe Daru::View::Table, 'table using daru-data_tables' do
   before { Daru::View.table_library = :datatables }
   before(:each) do
     @data_array = [[1, 15], [2, 30], [4, 40]]
-    @string_array = [[1, 15], ["daru", "view"]]
+    @string_array = ["daru", "view"]
 
     @data_vec1 = Daru::Vector.new([1 ,2, 4])
     @data_vec2 = Daru::Vector.new([15 ,30, 40])
