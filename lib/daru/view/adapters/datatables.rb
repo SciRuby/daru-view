2441a21e3f8431d1a3b6db5a3d83b5fbabcecef9
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-07-05 13:46:17 +0530

    Use javascript Array rather than html table in DataTables

--- lib/daru/view/adapters/datatables.rb
+++ lib/daru/view/adapters/datatables.rb
@@ -1,6 +1,7 @@
 require 'daru/data_tables'
 require 'daru'
 require 'securerandom'
+require 'erb'
 
 module Daru
   module View
@@ -16,16 +17,21 @@ module Daru
           # TODO : create data array from the df and vector data. So that
           # we can directly use the array.(No need to create df or vector and
           # generate the html table using to_html)
-          if data.is_a?(Array)
-            data_name = 'series_data'+ SecureRandom.uuid
-            data =
-              if data.all? { |e| e.class==Array }
-                Daru::DataFrame.rows(data, name: data_name)
-              else
-                Daru::Vector.new(data, name: data_name)
-              end
+          # if data.is_a?(Array)
+          #   data_name = 'series_data'+ SecureRandom.uuid
+          #   data =
+          #     if data.all? { |e| e.class==Array }
+          #       Daru::DataFrame.rows(data, name: data_name)
+          #     else
+          #       Daru::Vector.new(data, name: data_name)
+          #     end
+          # end
+          options[:data] = to_data_array(data)
+          row_number = 0
+          options[:data].each do |array|
+            array.unshift(row_number)
+            row_number += 1
           end
-          # options[:data] = data_in_array unless data_in_array.empty?
           @table = Daru::DataTables::DataTable.new(options)
           @data = data
           @table
@@ -40,12 +46,12 @@ module Daru
             class: 'display',
             cellspacing: '0',
             width: '100%',
-            table_html: @data.to_html_thead + @data.to_html_tbody
+            table_html: extract_table
           }
-          html_options ={
+          html_options = {
             table_options: table_opts
           }
-          table.to_html(@data.name, html_options)
+          table.to_html('series_data'+ SecureRandom.uuid, html_options)
         end
 
         def export_html_file(table, path='./table.html')
@@ -73,16 +79,36 @@ module Daru
           case
           when data_set.is_a?(Daru::DataFrame)
             return ArgumentError unless data_set.index.is_a?(Daru::Index)
-            data_set.access_row_tuples_by_indexs(*data_set.index.to_a)
+            rows = data_set.access_row_tuples_by_indexs(*data_set.index.to_a)
+            convert_to_array_of_array(rows)
           when data_set.is_a?(Daru::Vector)
             rows = []
             data_set.to_a.each { |a| rows << [a] }
             rows
           when data_set.is_a?(Array)
-            data_set
+            convert_to_array_of_array(data_set)
           else
             raise ArgumentError # TODO: error msg
           end
+        end
+
+        def convert_to_array_of_array(rows)
+          if rows.all? { |row| row.class==Array }
+            rows
+          else
+            tuples = []
+            rows.each { |row| tuples << [row] }
+            tuples
+          end
+        end
+
+        def extract_table
+          return @data.to_html_thead unless @data.is_a?(Array)
+          path = File.expand_path(
+            '../templates/datatables/thead.erb', __dir__
+          )
+          template = File.read(path)
+          ERB.new(template).result(binding)
         end
       end
     end
