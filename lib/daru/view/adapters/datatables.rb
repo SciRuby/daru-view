1dfc43ee71bcaf45e2d1998bc5a34b550051424b
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-07-12 04:20:21 +0530

    Improved API daru-data_tables

--- lib/daru/view/adapters/datatables.rb
+++ lib/daru/view/adapters/datatables.rb
@@ -14,14 +14,7 @@ module Daru
         #
         # TODO : this docs must be improved
         def init_table(data=[], options={})
-          options[:data] = to_data_array(data)
-          row_number = 0
-          options[:data].each do |array|
-            array.unshift(row_number)
-            row_number += 1
-          end
-          @table = Daru::DataTables::DataTable.new(options)
-          @table.data = data
+          @table = Daru::DataTables::DataTable.new(data, options)
           @table
         end
 
@@ -30,17 +23,7 @@ module Daru
         end
 
         def generate_body(table)
-          table_opts = {
-            class: 'display',
-            cellspacing: '0',
-            width: '100%',
-            table_html: extract_table(table)
-          }
-          html_options = {
-            table_options: table_opts
-          }
-          table.element_id ||= 'series_data'+ SecureRandom.uuid
-          table.to_html(table.element_id, html_options)
+          table.to_html
         end
 
         def export_html_file(table, path='./table.html')
@@ -66,47 +49,6 @@ module Daru
 
         def init_iruby
           Daru::DataTables.init_iruby
-        end
-
-        private
-
-        # DataTables accept the data as Array of array.
-        #
-        # TODO : I didn't find use case for multi index.
-        def to_data_array(data_set)
-          case
-          when data_set.is_a?(Daru::DataFrame)
-            return ArgumentError unless data_set.index.is_a?(Daru::Index)
-            rows = data_set.access_row_tuples_by_indexs(*data_set.index.to_a)
-            convert_to_array_of_array(rows)
-          when data_set.is_a?(Daru::Vector)
-            rows = []
-            data_set.to_a.each { |a| rows << [a] }
-            rows
-          when data_set.is_a?(Array)
-            convert_to_array_of_array(data_set)
-          else
-            raise ArgumentError # TODO: error msg
-          end
-        end
-
-        def convert_to_array_of_array(rows)
-          if rows.all? { |row| row.class==Array }
-            rows
-          else
-            tuples = []
-            rows.each { |row| tuples << [row] }
-            tuples
-          end
-        end
-
-        def extract_table(table)
-          return table.data.to_html_thead unless table.data.is_a?(Array)
-          path = File.expand_path(
-            '../templates/datatables/thead.erb', __dir__
-          )
-          template = File.read(path)
-          ERB.new(template).result(binding)
         end
       end
     end
