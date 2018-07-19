f527e77660c28d2e6b2655ce6d94edb85f77c59f
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-07-20 01:07:12 +0530

    Add pattern formatter

---
+++ lib/daru/view/adapters/googlecharts/formatters.rb
@@ -0,0 +1,19 @@
+module GoogleVisualr
+  class PatternFormat < Formatter
+    attr_accessor :des_col
+    def initialize(format_string)
+      @format_string = format_string
+    end
+
+    def src_cols=(*columns)
+      @src_cols = columns.flatten
+    end
+
+    def to_js
+      js = "\nvar formatter = new google.visualization.#{self.class.to_s.split('::').last}("
+      js << "'#{@format_string}'"
+      js << ');'
+      js << "\nformatter.format(data_table, #{@src_cols}, #{@des_col});"
+    end
+  end
+end
