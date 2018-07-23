3fe934451550bfbb4a7fa6accd39f69dc0b11a89
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-07-23 21:50:58 +0530

    Rename the method

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -61,4 +61,43 @@ describe Daru::View, "Daru::View class basic methods" do
     end
   end
 
+  context "#dependent_scripts" do
+    let(:script) {
+      Daru::View.dependent_scripts(
+        [:googlecharts, :nyaplot, :highcharts, :datatables]
+      )
+    }
+    it "generates dependent script of highcharts" do
+      expect(script).to match(/BEGIN highcharts.css/i)
+      expect(script).to match(/END highcharts.css/i)
+      expect(script).to match(/BEGIN js\/highstock.js/i)
+      expect(script).to match(/END js\/highstock.js/i)
+      expect(script).to match(/BEGIN modules\/exporting.js/i)
+      expect(script).to match(/END modules\/exporting.js/i)
+      expect(script).to match(/BEGIN highcharts-3d.js/i)
+      expect(script).to match(/END highcharts-3d.js/i)
+      expect(script).to match(/BEGIN modules\/data.js/i)
+      expect(script).to match(/END modules\/data.js/i)
+    end
+    it "generates dependent JS of googlecharts" do
+      expect(script).to match(/BEGIN google_visualr.js/)
+      expect(script).to match(/END google_visualr.js/)
+      expect(script).to match(/BEGIN loader.js/)
+      expect(script).to match(/END loader.js/)
+    end
+    it "generates dependent JS of nyaplot" do
+      expect(script).to match(/http:\/\/cdnjs.cloudflare.com/)
+      expect(script).to match(/"downloadable":"http:\/\/cdn.rawgit.com/)
+      expect(script).to match(/"d3":"https:\/\/cdnjs.cloudflare.com/)
+    end
+    it "generates dependent JS of datatables" do
+      expect(script).to match(/BEGIN jquery.dataTables.css/)
+      expect(script).to match(/END jquery.dataTables.css/)
+      expect(script).to match(/BEGIN jquery\-latest.min.js/)
+      expect(script).to match(/END jquery\-latest.min.js/)
+      expect(script).to match(/BEGIN jquery.dataTables.js/)
+      expect(script).to match(/END jquery.dataTables.js/)
+    end
+  end
+
 end
