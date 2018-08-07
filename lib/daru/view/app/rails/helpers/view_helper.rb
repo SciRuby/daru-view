81a295d6d2ab25a5435a858f295ed48bfe63ce0f
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-08-08 02:03:17 +0530

    Add user_options in daru_chart and daru_table

--- lib/daru/view/app/rails/helpers/view_helper.rb
+++ lib/daru/view/app/rails/helpers/view_helper.rb
@@ -11,12 +11,12 @@ module Daru
           helper_method('daru_chart', 'daru_table') if respond_to?(:helper_method)
         end
 
-        def daru_chart(data=[], options={})
-          Daru::View::Plot.new(data, options).div.html_safe
+        def daru_chart(data=[], options={}, user_options={})
+          Daru::View::Plot.new(data, options, user_options).div.html_safe
         end
 
-        def daru_table(data=[], options={})
-          Daru::View::Table.new(data, options).div.html_safe
+        def daru_table(data=[], options={}, user_options={})
+          Daru::View::Table.new(data, options, user_options).div.html_safe
         end
       end
     end
